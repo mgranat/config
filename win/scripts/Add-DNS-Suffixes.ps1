67a9ba43511f2c0990a5cf47a41bf8b5577ddad1
mgranat <max.granat@ni.com>
2021-01-30 16:14:10 -0600

    Remove extra newline.

--- win/scripts/Add-DNS-Suffixes.ps1
+++ win/scripts/Add-DNS-Suffixes.ps1
@@ -1,2 +1,1 @@
 Set-DnsClientGlobalSetting -SuffixSearchList @("natinst.com", "ni.corp.natinst.com", "ni.com", "amer.corp.natinst.com", "apac.corp.natinst.com", "emea.corp.natinst.com", "partners.natinst.com", "aws.natinst.com")
-
