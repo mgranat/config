7ffd2641de8e303ae21f97328b6a4c2faff65989
mgranat <max.granat@ni.com>
2021-01-30 16:12:37 -0600

    Add basic scripts.

---
+++ win/scripts/Disable-Autologon.ps1
@@ -0,0 +1,6 @@
+#Requires -RunAsAdministrator
+
+$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
